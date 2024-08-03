f1c98054378bbdbd5daa256a19ffbb8538c9c1a8
Roshan1998mishra <mishraroshan378@gmail.com>
2024-08-03 18:01:48 +0530

    First

---
+++ src/Components/First.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const First = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>API Integrate with Axios</h1>
+      <ul id='data'>
+        {data.map((item, index) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default First;
